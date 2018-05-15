b4e1d9aaeaeff87a898f9a5ca6696b722a99d564
RyanToo <37101651+RyanToo@users.noreply.github.com>
2018-05-15 12:32:17 +0500

    Update list.php

--- list.php
+++ list.php
@@ -2,10 +2,8 @@
 <p>Список тестов:</p>
 <?php
 foreach (glob("tests/*.json") as $i => $filename) {
-	$name = basename($filename);?>
-	
-
-<label ><a href="test.php?testname=<?= $name ?>">Выбрать тест</a> <?= ++$i, "</br>" ?> </label>
+  $name = basename($filename);?>
+<label ><a href="test.php?testname=<?= $name ?>">выбор теста</a> <?= ++$i, "</br>" ?> </label>
     
 <?php }?>
 
