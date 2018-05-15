6918e57a6695ec1c3201a24ccba674e561e2e81b
RyanToo <37101651+RyanToo@users.noreply.github.com>
2018-05-15 12:36:00 +0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,5 @@
 
 <!DOCTYPE html>
-<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Тест</title>
@@ -8,18 +7,17 @@
 <body>
 	<form enctype="multipart/form-data" action="admin.php" method="POST">
 		<input type="file" name="userfile">
-		<input type="submit" value="Отправить">
+		<input type="submit" value="Отправить файл">
 	</form>
 </body>
 </html>
-
-<?php
-$uploaddir = 'tests/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    print "Файл загружен." . "</br>";  
-    echo  '<a href="list.php">"Список тестов"</a>';
-} else {
-    print "Ошибка";
-}
-?>
+	<?php
+	$uploaddir = 'tests/';
+	$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+	    print "Файл был загружен." . "</br>";  
+	    echo  '<a href="list.php">"Список тестов"</a>';
+	} else {
+	    print "Ошибка";
+	}
+	?>
